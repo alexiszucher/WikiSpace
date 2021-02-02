0c6b72cba4b1f38ff02bacbeaa514193db798c01
alexiszucher <48800960+alexiszucher@users.noreply.github.com>
2021-02-03 00:27:01 +0100

    ff

--- pages/index.php
+++ pages/index.php
@@ -1,17 +1,19 @@
 <?php require_once "header.php"; ?>
 
-<div align="center" class="col-6 margin-top-30">
+<div align="center" class="col-4 margin-top-30">
     <a href="php.php"><button class="btn btn-success">Documentation php</button></a>
     <br /><br />
     <a href="apache-faire-vhost.php"><button class="btn btn-success">Apache : Faire un vhost !</button></a>
-    <br /><br />
-    <a href="ssh.php"><button class="btn btn-success">Documentation SSH</button></a>
 </div>
-<div align="center" class="col-6 margin-top-30">
+<div align="center" class="col-4 margin-top-30">
+    <a href="js.php"><button class="btn btn-success">Documentation JavaScript</button></a>
     <br /><br />
     <a href="ssh.php"><button class="btn btn-success">Documentation GIT</button></a>
-    <br /><br />
+</div>
+<div align="center" class="col-4 margin-top-30">
     <a href="redis.php"><button class="btn btn-success">Documentation Redis</button></a>
+    <br /><br />
+    <a href="ssh.php"><button class="btn btn-success">Documentation SSH</button></a>
 </div>
 <div align="center" class="col-12 margin-top-30">
     <br /><br /><br /><br />
---
+++ pages/js.php
@@ -0,0 +1,26 @@
+<?php require_once "header.php"; ?>
+
+<h2>Documentation JavaScript</h2>
+
+<div class="col-12">
+    <div tag="CREATION DASHBOARD GLISSER DEPOSER">
+        <b>CREATION DASHBOARD GLISSER DEPOSER</b>
+        <br /><br />
+        <textarea cols="100" rows="7">
+            Utiliser la librairie gridstack
+        </textarea>
+    </div>
+
+    <br /><br />
+
+    <div tag="INTEGRATION GOOGLE MAP CUSTOMIZE">
+        <b>INTEGRATION GOOGLE MAP CUSTOMIZE</b>
+        <br /><br />
+        <textarea cols="100" rows="7">
+            Utiliser la librairie leaflet
+        </textarea>
+    </div>
+</div>
+
+<?php require_once "footer.php"; ?>
+
