086487110ad30081f1a2354f7ba75dc8cdaad425
Alexis Zucher <a.zucher@girondenumerique.fr>
2021-02-05 21:46:04 +0100

    ss

--- pages/compte-rendu-alternance.php
+++ pages/compte-rendu-alternance.php
@@ -3,10 +3,27 @@
 <div class="col-12 white">
     <h2><b>3 fev 2021</b></h2>
     <br/>
+    <b>SMART CITY : PATCH DES ERREURS D'ENCODAGE</b>
+    <br/>
     <b>SMART CITY : UPLOAD IMAGE ORGANISATION</b>
     <br/>
     <b>SMART CITY : DOCUMENTATION GITLAB REDIS + MODULE JSON</b>
+    <br/>
+    <b>AVANCEMENT REFONTE GAAPSE</b>
 </div>
+
+<br />
+A FAIRE : 
+<br />
+- Changer CSS SMART city avec mt-1, mt-2, mt-3
+<br />
+- JS à changer SMART CITY
+<br />
+- GAAPSE SLIM : POSITION CSS top index à fix
+<br />
+- ADMIN GAAPSE SLIM : AFFICHER TOUS LES DOMAINES 
+
+
 
 <?php require_once "footer.php"; ?>
 
